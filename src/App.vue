2a8a06d2cb13abc5c27829b2613374c376c090c5
Jake Silvers <steaksilvers@yahoo.com>
2023-04-02 17:50:06 -0400

    test page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/img_1696_720.jpg">
+    <HelloWorld msg="Coming Soon..."/>
   </div>
 </template>
 
